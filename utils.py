cba31099b7886e783eb96498491cf0f832b5cee9
Abdullah Bahi <abdullahbahi@icloud.com>
2024-11-20 11:03:08 +0200

    testing

--- utils.py
+++ utils.py
@@ -351,6 +351,8 @@ def calculate_installments(unit_info, tenor_years, payment_frequency, contract_d
     # Calculate gas payments 
     print(project_policy['gas_policy']['num_pmts'], periods_per_year/base_periods_per_year)
     project_policy['gas_policy']['num_pmts'] = int(round(project_policy['gas_policy']['num_pmts'] * periods_per_year/base_periods_per_year))
+    if project_policy['gas_policy']['num_pmts'] < 1:
+        project_policy['gas_policy']['num_pmts'] = 1
     print(project_policy['gas_policy']['num_pmts'])
     if project_policy['gas_policy']['is_applied']:
         gas_payments = calculate_gas_payments(project_policy['gas_policy'], tenor_years, periods_per_year, contract_date, unit_info['Delivery Date'])
