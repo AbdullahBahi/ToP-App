26779911cac497f38fadf6398d485fd86df5d5ac
Abdullah Bahi <abdullahbahi@icloud.com>
2024-11-20 17:55:42 +0200

    testing

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ from flask import Flask, request, jsonify, json
 from utils import calculate_installments, get_units_data
 
 app = Flask(__name__)
-# Disable global JSON sorting
-app.config['JSON_SORT_KEYS'] = False
 
 @app.route('/calculate_installments', methods=['POST'])
 def calculate_installments_api():
