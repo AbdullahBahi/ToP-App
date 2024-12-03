8f45034e41acc6c3466f2e66d4db4a6c7014878a
Abdullah Bahi <abdullahbahi@icloud.com>
2024-12-03 17:17:30 +0200

    added offsets to gas calculation

--- utils.py
+++ utils.py
@@ -95,23 +95,34 @@ def caclulate_years_till_delivery(contract_date, delivery_date):
 def calculate_gas_payments(policy, tenor_years, periods_per_year, contract_date, delivery_date):
     num_pmts = policy['num_pmts']
     scheduling = policy['scheduling']
+
+    years_till_delivery = caclulate_years_till_delivery(contract_date, delivery_date)
+
+    # Select gas fee
     fees = policy['fees']
     fees = {float(k):v for k, v in fees.items()}
-    
-    # Select gas fee
-    years_till_delivery = caclulate_years_till_delivery(contract_date, delivery_date)
+
     diffs = {abs(years_till_delivery-k):v for k, v in fees.items()}
     gas_fee = diffs[min(diffs.keys())]
 
+    # Select offset
+    offsets = policy['offsets']
+    offsets = {float(k):v for k, v in offsets.items()}
+
+    diffs = {abs(years_till_delivery-k):v for k, v in offsets.items()}
+    offset = offsets[min(diffs.keys())] * periods_per_year
+
     # Schedule gas fee
     delivery_payment_index = round(years_till_delivery * periods_per_year)
-    
+
+    if delivery_payment_index - offset > 0:
+        delivery_payment_index = delivery_payment_index - offset
+
     if years_till_delivery > tenor_years:
         n = delivery_payment_index
     else:
         n = tenor_years * periods_per_year
     
-    n = tenor_years * periods_per_year
     gas_payments = [0,]*(n+1)
 
     if scheduling == "at_delivery":
